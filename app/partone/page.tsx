efedb0667741737efd2cc6277fa9a7dbbf753c07
BirkBerger <43404623+BirkBerger@users.noreply.github.com>
2026-03-11 15:41:16 +0100

    Add Toggle, LoadingInput and FigmaButton to Part 1.

--- app/partone/page.tsx
+++ app/partone/page.tsx
@@ -1,4 +1,46 @@
+'use client'
+
+import { useState } from 'react';
+import FigmaButton from '../components/FigmaButton';
+import Toggle from '../components/Toggle';
+import LoadInput from '../components/LoadInput';
+
 function PartOne() {
+
+    const [disabled, setDisabled] = useState(true);
+    const [boring, setBoring] = useState(true);
+    const [loadingSeconds, setLoadingSeconds] = useState(0);
+
+    return (
+        <div className="flex flex-col grow gap-10">
+
+            <div className="flex flex-col gap-5 w-full max-w-[1000px] self-center">
+                <Toggle
+                    leftLabel="Disabled"
+                    rightLabel="Clickable"
+                    isLeft={disabled}
+                    onToggle={() => setDisabled(!disabled)}
+                ></Toggle>
+                <Toggle
+                    leftLabel="Boring"
+                    rightLabel="Exciting"
+                    isLeft={boring}
+                    onToggle={() => setBoring(!boring)}
+                ></Toggle>
+                <LoadInput
+                    onGo={(seconds) => setLoadingSeconds(seconds)}
+                ></LoadInput>
+            </div>
+
+            <div className="flex justify-center">
+                <FigmaButton
+                    disabled={disabled}
+                    boring={boring}
+                    loadingSeconds={loadingSeconds}
+                ></FigmaButton>
+            </div>
+        </div>
+    ) 
 }
 
 export default PartOne;
